7dd7421da71624317f13f0444d92b0d5bcb0d846
Rob Morgan <rmorgan323@gmail.com>
2018-02-05 14:47:41 -0700

    add database seed files and basic structure

---
+++ db/data/terms.js
@@ -0,0 +1,232 @@
+const terms = [
+  {
+    term: 'JRC/AR',
+    definition: `This is the abbreviation for Just Right Challenge and Adaptive Response. Since we are utilizing and ASI© model as one foundational frame of reference, the treatment plan needs to include mindful decisions about how the therapists might guide the JRC and what AR's are noted.  This is where you should note what treatment was offered and what the response was.  In the treatment planning section, do this related to the STEPSI© components.`,
+    category_name: 'Spirit',
+    imageURL:
+  },
+  {
+    term: 'S-A-M',
+    definition: `This is an abbreviation for Sensory-Affective-Motor, and was originated by Greenspan. For the SpIRiT©, Stackhouse and colleagues have elaborated the Sensory-Affective-Motor function into the low and high routes of processing that occur in each function.  This is the pivotal function that helps to 'connect the dots and trace the threads' of connection between the functions in the SpIRiT© model.  The idea here is for the clinician to select which aspect seems to be the "driver" of the concerns in that area (S-A-M) - it is a clinical decision/tacit response about what seems to be the most primary. This should be consistent, then, with what your clinical decisions are in each area.  It could be one, two, or all three (S-A-M), and it could vary from one area to another.`,
+    category_name: 'Spirit',
+    imageURL: true
+  },
+  {
+    term: 'S in S-A-M: Sensory',
+    definition: `High: Sensory Discrimination, Low: Sensory Modulation`,
+    category_name: 'Spirit',
+    imageURL:
+  },
+  {
+    term: 'A in S-A-M: Affective',
+    definition: `High: Motivational Biasing System, Low: Affective Valence`,
+    category_name: 'Spirit',
+    imageURL:
+  },
+  {
+    term: 'M in S-A-M: Motor',
+    definition: `High: Motor Planning Executive system, Low: Automatic postural and motor functions`,
+    category_name: 'Spirit'
+    imageURL:
+  },
+  {
+    term: 'Vestibular',
+    definition: 'Receptors located bilaterally in inner ears; receives information about gravity and movement  in all planes and detects change, variation, speed, location of the movement; partners with proprioception and vision to give sense of balance and postural adaptation; participates with cranial nerves to control ocular motor function and integrate head/neck movements with body and space; gives rise to bilateral control, spatial awareness, body awareness, movement and balance, contributes to sense of self and sense of here/now and organizes for anticipation and future planning; senses vibration and wellness of self and other',
+    category_name: 'Sensory Systems',
+    imageURL:
+  },
+  {
+    term: ''
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  {
+    term:
+    definition: 
+    category_name:
+    imageURL:
+  },
+  
+]
+
