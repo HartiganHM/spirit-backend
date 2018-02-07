ad46e9fe7ec60cd62217dd0964205671e234362b
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-06 20:25:54 -0700

    Add production environment to knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -26,5 +26,14 @@ module.exports = {
     useNullAsDefault: true
   },
 
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  }
+
 
 };
