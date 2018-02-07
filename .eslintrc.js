50bb29e1cab2952b66345029ccec7b99138238c6
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-06 20:06:01 -0700

    Remove eslint-react plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,7 @@
 module.exports = {
   "parser": "babel-eslint",
   "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended"
-  ],
-  "plugins": [
-    "react"
+    "eslint:recommended"
   ],
   "env": {
     "browser": true,
