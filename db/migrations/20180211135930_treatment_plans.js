1a325e911698494d0836a93808658b8ab0350f7c
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-11 14:04:49 -0700

    Build treatment_plans table

---
+++ db/migrations/20180211135930_treatment_plans.js
@@ -0,0 +1,22 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('treatment_plans', function(table) {
+      table.increments('id').primary();
+      table.string('category');
+      table.text('sensory', 'longtext');
+      table.text('task', 'longtext');
+      table.text('environment', 'longtext');
+      table.text('predictability', 'longtext');
+      table.text('self_regulation', 'longtext');
+      table.text('interaction', 'longtext');
+      table.text('JRC_AR_notes', 'longtext');
+      table.integer('session_id').unsigned();
+      table.foreign('session_id').references('sessions.id');
+      table.timestamps(true, true);
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([knex.schema.dropTable('treatment_plans')]);
+};
