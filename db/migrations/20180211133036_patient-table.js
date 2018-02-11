902f8b438dcdf060f777edfec850507e1a127187
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-11 13:36:47 -0700

    Build patients table

---
+++ db/migrations/20180211133036_patient-table.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('patients', function(table) {
+      table.increments('id').primary();
+      table.string('abstracted_name');
+      table.string('clinic_name');
+      table.integer('ot_id').unsigned();
+      table.foreign('ot_id').references('users.id');
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([knex.schema.dropTable('patients')]);
+};
