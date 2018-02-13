b42dfea2f2903c9a2684975550b30d357b566c8d
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-13 16:43:33 -0700

    Write POST for new primary concern and PUT to update primary concern

--- server.js
+++ server.js
@@ -409,6 +409,49 @@ app.post('/api/v1/users/:user_id/patients', async (request, response) => {
     });
 });
 
+//////  CREATE NEW PRIMARY CONCERN //////
+// NOTE:  Requires patient id in params and all primary_concern propertis in body.
+//        Call will add the patient_id to primary concern.
+app.post(
+  '/api/v1/patients/:patientId/primary-concerns',
+  async (request, response) => {
+    const newPrimaryConcern = request.body;
+    const { patientId } = request.params;
+
+    for (let requiredParameter of ['description']) {
+      if (!newPrimaryConcern[requiredParameter]) {
+        return response
+          .status(422)
+          .json({ error: `Missing required parameter - ${requiredParameter}` });
+      }
+    }
+
+    const patientName = await database('patients')
+      .where('id', patientId)
+      .select();
+
+    if (!patientName.length) {
+      return response
+        .status(404)
+        .json({ error: `Patient by id ${patientId} not found.` });
+    }
+
+    const addPrimaryConcern = await Object.assign({}, newPrimaryConcern, {
+      patient_id: patientId
+    });
+
+    database('primary_concerns')
+      .returning('id')
+      .insert(addPrimaryConcern)
+      .then(id => {
+        return response.status(201).json(id);
+      })
+      .catch(error => {
+        return response.status(500).json({ error });
+      });
+  }
+);
+
 //////  CREATE NEW TERM //////
 // NOTE:  Requires category id in params and then term and definition in body.
 //        Call will add the category name to the term.
@@ -445,6 +488,31 @@ app.post('/api/v1/categories/:category_id/terms', async (request, response) => {
       return response.status(500).json({ error });
     });
 });
+
+//////  UPDATE PRIMARY CONCERN //////
+app.put('/api/v1/primary-concerns/:primaryConcernId', async (request, response) => {
+  const { patientId, primaryConcernId } = request.params;
+  const updatedPrimaryConcern = request.body;
+  const primaryConcernToUpdate = await database('primary_concerns')
+    .where('id', primaryConcernId)
+    .select();
+
+  if(!primaryConcernToUpdate.length) {
+    return response.status(422).json({ error: `Primary concern ${primaryConcernId} not found.` });
+  }
+
+  await database('primary_concerns')
+    .where('id', primaryConcernId)
+    .update(updatedPrimaryConcern)
+    .then(() => {
+      return response.status(201).send({
+        success: `Primary concern ${primaryConcernId} updated.`
+      });
+    })
+    .catch(error => {
+      return response.status(500).json({ error })
+    })
+})
 
 //////  UPDATE TERM //////
 app.put('/api/v1/terms/:terms_id', async (request, response) => {
