44169d3657a923b701c38443c6837c83aa04491a
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-13 19:36:15 -0700

    Write successful PUT test for new primary concern

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -738,7 +738,28 @@ describe('API Routes', () => {
     });
 
     it('Should add a new primary concern to a patient', () => {
-
+      return chai
+        .request(server)
+        .post('/api/v1/patients/1/primary-concerns')
+        .send(
+          {
+            description: 'Does not play well at school',
+            domain_1: true,
+            domain_2: false,
+            domain_3: true,
+            domain_4: true,
+            domain_5: false,
+            domain_6: true,
+            notes: 'Plays well with parents around, but not while away'
+          }
+        )
+        .then(response => {
+          response.should.have.status(201);
+          response.should.be.json;
+        })
+        .catch(error => {
+          throw error;
+        });
     });
 
     it('Should return a 422 error if missing a required parameter', () => {
