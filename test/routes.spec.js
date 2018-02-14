b7bb27fb886c2fc6eb3ce0188a3432351c22551c
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-13 20:27:11 -0700

    Write test for 404 error of GET primary concern by id

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -301,7 +301,19 @@ describe('API Routes', () => {
     });
 
     it('Should return a 404 error if primary concern is not found', () => {
-
+      return chai
+        .request(server)
+        .get('/api/v1/primary-concerns/0')
+        .then(response => {
+          response.should.have.status(404);
+          response.should.be.json;
+          response.error.text.should.equal(
+            '{"error":"Primary concern 0 not found."}'
+          );
+        })
+        .catch(error => {
+          throw error;
+        });
     });
   });
 
