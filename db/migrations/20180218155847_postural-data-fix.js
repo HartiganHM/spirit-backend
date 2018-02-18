a0545de2d8425c8fa30f624c372e2f8f2fc71e75
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-18 16:08:32 -0700

    Build migration to only drop columns

--- db/migrations/20180218155847_postural-data-fix.js
+++ db/migrations/20180218155847_postural-data-fix.js
@@ -3,8 +3,6 @@ exports.up = function(knex, Promise) {
     knex.schema.table('processes', function(table) {
       table.dropColumn('pos_1_outer');
       table.dropColumn('pos_1_inner');
-      table.boolean('pos_1_outer');
-      table.boolean('pos_1_inner');
     })
   ]);
 };
@@ -12,8 +10,6 @@ exports.up = function(knex, Promise) {
 exports.down = function(knex, Promise) {
   return Promise.all([
     knex.schema.table('processes', function(table) {
-      table.dropColumn('pos_1_outer');
-      table.dropColumn('pos_1_inner');
       table.string('pos_1_outer');
       table.string('pos_1_inner');
     })
