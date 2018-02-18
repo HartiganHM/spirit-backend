ae4bf6030587623eb9111d149a85723a8a58662f
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-18 16:02:05 -0700

    Add Promise.all to migration

--- db/migrations/20180218155847_postural-data-fix.js
+++ db/migrations/20180218155847_postural-data-fix.js
@@ -1,17 +1,21 @@
 exports.up = function(knex, Promise) {
-  knex.schema.table('processes', function(table) {
-    table.dropColumn('pos_1_outer');
-    table.dropColumn('pos_1_inner');
-    table.boolean('pos_1_outer');
-    table.boolean('pos_1_inner');
-  });
+  return Promise.all([
+    knex.schema.table('processes', function(table) {
+      table.dropColumn('pos_1_outer');
+      table.dropColumn('pos_1_inner');
+      table.boolean('pos_1_outer');
+      table.boolean('pos_1_inner');
+    })
+  ]);
 };
 
 exports.down = function(knex, Promise) {
-  knex.schema.table('processes', function(table) {
-    table.dropColumn('pos_1_outer');
-    table.dropColumn('pos_1_inner');
-    table.string('pos_1_outer');
-    table.string('pos_1_inner');
-  });
+  return Promise.all([
+    knex.schema.table('processes', function(table) {
+      table.dropColumn('pos_1_outer');
+      table.dropColumn('pos_1_inner');
+      table.string('pos_1_outer');
+      table.string('pos_1_inner');
+    })
+  ]);
 };
