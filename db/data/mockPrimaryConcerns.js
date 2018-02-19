d090050a450d20aff8a7db26d01231a16795aca6
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-19 14:56:59 -0700

    Build mockPrimaryConcerns data object

---
+++ db/data/mockPrimaryConcerns.js
@@ -0,0 +1,14 @@
+const primaryConcerns = [
+  {
+    description: 'Does not play well at school',
+    domain_1: true,
+    domain_2: false,
+    domain_3: true,
+    domain_4: true,
+    domain_5: false,
+    domain_6: true,
+    notes: 'Plays well with parents around, but not while away'
+  }
+];
+
+module.exports = primaryConcerns;
