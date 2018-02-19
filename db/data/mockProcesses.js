86a7105333ef34da35b8f084c338bd438628498d
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-19 14:58:51 -0700

    Build mockProcesses data object

---
+++ db/data/mockProcesses.js
@@ -0,0 +1,11 @@
+const processes = [
+  {
+    sen_h_vestibular: '7F',
+    mod_2_autonomic: '3R',
+    exe_4b_self_control: '5A',
+    pos_5_alignment_COG: '10F',
+    soc_2_social_motivators: '3I'
+  }
+];
+
+module.exports = processes;
