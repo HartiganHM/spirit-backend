4b758f31e2af438f565f96eb2581c1aab19a32f9
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-19 14:48:36 -0700

    Update test seed treatmentPlans data object

--- db/seeds/test/terms.js
+++ db/seeds/test/terms.js
@@ -55,14 +55,57 @@ const processes = [
 
 const treatmentPlans = [
   {
-    category: 'Sensory',
-    task: 'Puzzle Games',
-    environment: 'Solo play in quiet setting',
-    predictability:
+    modulation_sensory: 'Needs help recognizing social cues',
+    modulation_task: 'Puzzle Games',
+    modulation_environment: 'Solo play in quiet setting',
+    modulation_predictability:
       'Should start self-sufficiently, but rely on OT as puzzles become more difficult',
-    self_regulation: 'Focus and attention',
-    interaction: 'Encourage problem solving with guidance',
-    JRC_AR_notes: 'Record results from distance, but be engaged if needed'
+    modulation_self_regulation: 'Focus and attention',
+    modulation_interaction: 'Encourage problem solving with guidance',
+    modulation_JRC_AR_notes:
+      'Record results from distance, but be engaged if needed',
+
+    posture_sensory: 'Needs help recognizing social cues',
+    posture_task: 'Puzzle Games',
+    posture_environment: 'Solo play in quiet setting',
+    posture_predictability:
+      'Should start self-sufficiently, but rely on OT as puzzles become more difficult',
+    posture_self_regulation: 'Focus and attention',
+    posture_interaction: 'Encourage problem solving with guidance',
+    posture_JRC_AR_notes:
+      'Record results from distance, but be engaged if needed',
+
+    sensory_discrimination_sensory: 'Needs help recognizing social cues',
+    sensory_discrimination_task: 'Puzzle Games',
+    sensory_discrimination_environment: 'Solo play in quiet setting',
+    sensory_discrimination_predictability:
+      'Should start self-sufficiently, but rely on OT as puzzles become more difficult',
+    sensory_discrimination_self_regulation: 'Focus and attention',
+    sensory_discrimination_interaction:
+      'Encourage problem solving with guidance',
+    sensory_discrimination_JRC_AR_notes:
+      'Record results from distance, but be engaged if needed',
+
+    social_emotional_sensory: 'Needs help recognizing social cues',
+    social_emotional_task: 'Puzzle Games',
+    social_emotional_environment: 'Solo play in quiet setting',
+    social_emotional_predictability:
+      'Should start self-sufficiently, but rely on OT as puzzles become more difficult',
+    social_emotional_self_regulation: 'Focus and attention',
+    social_emotional_interaction: 'Encourage problem solving with guidance',
+    social_emotional_JRC_AR_notes:
+      'Record results from distance, but be engaged if needed',
+
+    executive_functioning_sensory: 'Needs help recognizing social cues',
+    executive_functioning_task: 'Puzzle Games',
+    executive_functioning_environment: 'Solo play in quiet setting',
+    executive_functioning_predictability:
+      'Should start self-sufficiently, but rely on OT as puzzles become more difficult',
+    executive_functioning_self_regulation: 'Focus and attention',
+    executive_functioning_interaction:
+      'Encourage problem solving with guidance',
+    executive_functioning_JRC_AR_notes:
+      'Record results from distance, but be engaged if needed'
   }
 ];
 
