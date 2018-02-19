e9922e9de36d9ed78ccd2d7e34ee67ca797d5453
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-19 14:57:42 -0700

    Build mockTherapyGoals data object

---
+++ db/data/mockTherapyGoals.js
@@ -0,0 +1,45 @@
+const therapyGoals = [
+  {
+    modulation_goal: 'Goal 1',
+    modulation_ot_importance: 5,
+    modulation_parent_importance: 5,
+    modulation_ot_performance: 5,
+    modulation_parent_performance: 5,
+    modulation_ot_satisfaction: 5,
+    modulation_parent_satisfaction: 5,
+
+    posture_goal: 'Goal 2',
+    posture_ot_importance: 5,
+    posture_parent_importance: 5,
+    posture_ot_performance: 5,
+    posture_parent_performance: 5,
+    posture_ot_satisfaction: 5,
+    posture_parent_satisfaction: 5,
+
+    sensory_discrimination_goal: 'Goal 3',
+    sensory_discrimination_ot_importance: 5,
+    sensory_discrimination_parent_importance: 5,
+    sensory_discrimination_ot_performance: 5,
+    sensory_discrimination_parent_performance: 5,
+    sensory_discrimination_ot_satisfaction: 5,
+    sensory_discrimination_parent_satisfaction: 5,
+
+    social_emotional_goal: 'Goal 4',
+    social_emotional_ot_importance: 5,
+    social_emotional_parent_importance: 5,
+    social_emotional_ot_performance: 5,
+    social_emotional_parent_performance: 5,
+    social_emotional_ot_satisfaction: 5,
+    social_emotional_parent_satisfaction: 5,
+
+    executive_functioning_goal: 'Goal 5',
+    executive_functioning_ot_importance: 5,
+    executive_functioning_parent_importance: 5,
+    executive_functioning_ot_performance: 5,
+    executive_functioning_parent_performance: 5,
+    executive_functioning_ot_satisfaction: 5,
+    executive_functioning_parent_satisfaction: 5
+  }
+];
+
+module.exports = therapyGoals;
