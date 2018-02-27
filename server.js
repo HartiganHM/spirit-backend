2b3e90c09c8ce8176661674f6379e405588e7802
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-27 16:12:55 -0700

    Add heroku app to allowedOrigins

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const database = require('knex')(configuration);
 const key = require('./pubKey');
 
 const corsOptions = {
-  allowedOrigins: ['localhost:3001', 'localhost:3000', 'rmorgan323.github.io'],
+  allowedOrigins: ['localhost:3001', 'localhost:3000', 'rmorgan323.github.io', 'spirit-fe.herokuapp.com'],
   preflightContinue: true,
   headers: ['Content-Type', 'x-token']
 };
